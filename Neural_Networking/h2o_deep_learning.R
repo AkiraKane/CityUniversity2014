544878eea585ad1ce86749887f18ce5a1b311a6d
Daniel Dixey <dan.dixey@gmail.com>
2015-02-11 22:26:09 +0000

    Update h2o_deep_learning.R

--- Neural_Networking/h2o_deep_learning.R
+++ Neural_Networking/h2o_deep_learning.R
@@ -11,6 +11,18 @@
 ## Initialise R environment
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+## Unhide if h2o is not install on your machine
+
+## Set the CRAN mirror
+local({r <- getOption("repos"); r["CRAN"] <- "http://cran.us.r-project.org"; options(repos = r)})
+
+## The following two commands remove any previously installed H2O packages for R.
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+## Install Package
+install.packages("h2o", repos=(c(paste(paste("http://s3.amazonaws.com/h2o-release/h2o/master/",version,sep=""),"/R",sep=""), getOption("repos"))))
+
 ## Import the h2o Library
 suppressMessages(library(h2o))
 suppressMessages(library(caret))
@@ -19,11 +31,17 @@ suppressMessages(library(caret))
 ## Initialise H2O Connection
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+## Run the demonstaration
+#demo(h2o.glm)
+
 ## Start a h2o Cluster
-localH2O <- h2o.init(ip = 'localhost', port = 54321, max_mem_size = '4g', nthreads=-1, )
+localH2O <- h2o.init(ip = 'localhost', port = 54321, max_mem_size = '4g', nthreads=-1)
 
 ## Check h2o cluster is functioning correctly
 h2o.clusterInfo(localH2O)
+
+# Check the current status
+h2o.clusterStatus(localH2O)
 
 ## =============================================================================
 ## Import Data
@@ -53,7 +71,7 @@ random <- h2o.runif(data, seed = 123456789)
 
 ## Create 3 Sets of Data
 train_hex <- h2o.assign(data[random < .9,], "train_set")
-#validation_hex  <- h2o.assign(data[random >= .7 & random < .8,], "validation_set")
+validation_hex  <- h2o.assign(data[random >= .7 & random < .8,], "validation_set")
 test_hex  <- h2o.assign(data[random >= .9,], "test_set")
 
 # Save as Dataframes
@@ -73,15 +91,16 @@ model <-
   h2o.deeplearning(x = c(1,2,3,4,5,6,7,8,12),  # column numbers for predictors
                    y = 17,   # column number for label
                    data = train_hex, # data in H2O format
-                   key = 'Deep_Learning_Model_x_1', # Assign a key value to Model
-                   activation = "Rectifier", # or 'Tanh'
+                   key = 'Deep_Learning_Model_x_1_1', # Assign a key value to Model
+                   activation = "RectifierWithDropout", # or 'Tanh'
+                   # validation = validation_hex, # Insert the Validation Dataset
                    classification = T, # Classification Task
-                   nfolds = 0, # Number of Folds
+                   nfolds = 10, # Number of Folds
                    #input_dropout_ratio = 0.2, # % of inputs dropout
-                   #hidden_dropout_ratios = c(0.5,0.5), # % for nodes dropout
-                   balance_classes = TRUE, 
-                   hidden = c(150,150,100,50), # two layers at 30 nodes
-                   epochs = 300, # max. no. of epochs
+                   #hidden_dropout_ratios = c(0.1,0.1), # % for nodes dropout
+                   l2 = 0.00001, # L2 regularization value
+                   hidden = c(50,50), # two layers at 30 nodes
+                   epochs = 200, # max. no. of epochs
                    variable_importances=T, # Get Variable Importance
                    train_samples_per_iteration = -1) 
 
@@ -99,30 +118,20 @@ model@model$varimp
 model@model$confusion
 
 ## Make prediction with trained model (on training data for simplicity), prediction is stored in H2O cluster
-prediction = h2o.predict(model, newdata = test_hex)
+prediction_test = h2o.predict(model, newdata = test_hex[,c(1,2,3,4,5,6,7,8,12)])
+prediction_train = h2o.predict(model, newdata = train_hex[,c(1,2,3,4,5,6,7,8,12)])
 
-## Save results in Dataframe
-res <- data.frame(Training = NA, Test = NA)
 
-## Evaluate performance
-yhat_train <- h2o.predict(model, train_hex)$predict
-yhat_train <- as.factor(as.matrix(yhat_train))
-yhat_test <- h2o.predict(model, test_hex)$predict
-yhat_test <- as.factor(as.matrix(yhat_test))
+## Get the confusion Matrix - Predicted Confustion Matrix
+confusion_pred = h2o.confusionMatrix(prediction_test[,1], test_hex[,17])
+confusion_pred
 
-## Print Confusion Matrix
-confusionMatrix(yhat_train, y_train)$table
-confusionMatrix(yhat_test, y_test)$table
-
-## Save Results into Dataframe
-res[1, 1] <- round(confusionMatrix(yhat_train, y_train)$overall[1], 4) # Save Accuracy for Training Set
-res[1, 2] <- round(confusionMatrix(yhat_test, y_test)$overall[1], 4) # Save Accuracy for Test Set
-
-## Print Results
-res
+# Get the confusion Matrix - Training Confustion Matrix
+confusion_train = h2o.confusionMatrix(prediction_train[,1], train_hex[,17])
+confusion_train
 
 ## =============================================================================
-## Pack-up and Time for Bed
+## Pack-up
 ## =============================================================================
 
 ## Check h2o cluster is functioning correctly
@@ -135,45 +144,42 @@ h2o.shutdown(localH2O)
 ## Results
 ## =============================================================================
 
-# > model
-# IP Address: localhost 
-# Port      : 54321 
-# Parsed Data Key: train_set 
-# 
-# Deep Learning Model Key: Deep_Learning_Model_x_1
-# 
-# Training classification error: 0.02326287
-# 
-# Validation classification error: 1
-# 
-# Confusion matrix:
-#   Reported on train_set 
+# > ## Display Variable importance
+#   > model@model$varimp
+# duration housing.yes default.no  loan.yes default.yes   balance job.student   loan.no job.admin. education.tertiary job.housemaid
+# 1        1   0.7993869  0.7794384 0.7032358   0.6051757 0.5849366   0.5163379 0.4709486  0.4518152           0.433008     0.3944367
+# marital.single marital.married job.retired       age education.primary housing.no job.unemployed job.technician job.self-employed
+# 1      0.3882895       0.3873966   0.3858609 0.3807179         0.3801363  0.3686665      0.3658478      0.3596107         0.3582516
+# job.entrepreneur education.secondary marital.divorced job.management job.unknown job.blue-collar education.unknown job.services
+# 1        0.3505748            0.345786        0.3436916      0.3357938    0.333468        0.330276         0.3234178    0.2974606
+# > 
+#   > ## Show the Confusion Matrix
+#   > model@model$confusion
 # Predicted
-# Actual     no  yes Error
-# no     4802  157 0.032
-# yes      73 4855 0.015
-# Totals 4875 5012 0.023
-# 
-# Relative Variable Importance:
-#   duration balance       age job.technician education.tertiary job.admin. marital.divorced job.management education.primary job.services
-# 1        1 0.88573 0.6125771       0.469664          0.4685727  0.4657619        0.4529966      0.4411596         0.4399247    0.4314883
-# job.retired job.self-employed housing.yes  loan.yes marital.single education.unknown education.secondary job.blue-collar housing.no
-# 1    0.431256         0.4268847    0.420892 0.4200836      0.4167744         0.4148537           0.4107296       0.4064752  0.4045341
-# marital.married job.housemaid job.unemployed job.student job.entrepreneur default.no   loan.no job.unknown default.yes
-# 1        0.399872     0.3956018       0.385637   0.3685814        0.3609788  0.3564793 0.3514062   0.2936039   0.2727928
-# 
-# AUC =  0.9968182 (on train)
-
-# Training   Test
-# 1   0.9793 0.8443
-
-# confusionMatrix(yhat_train, y_train)$table
-# Reference
-# Prediction    no   yes
-# no  35235   216
-# yes   624  4544
-# > confusionMatrix(yhat_test, y_test)$table
-# Reference
-# Prediction   no  yes
-# no  3684  336
-# yes  379  193
+# Actual      no  yes Error
+# no     31960 3899 0.109
+# yes     1864 2896 0.392
+# Totals 33824 6795 0.142
+# > 
+#   > ## Make prediction with trained model (on training data for simplicity), prediction is stored in H2O cluster
+#   > prediction_test = h2o.predict(model, newdata = test_hex[,c(1,2,3,4,5,6,7,8,12)])
+# > prediction_train = h2o.predict(model, newdata = train_hex[,c(1,2,3,4,5,6,7,8,12)])
+# > 
+#   > 
+#   > ## Get the confusion Matrix - Predicted Confustion Matrix
+#   > confusion_pred = h2o.confusionMatrix(prediction_test[,1], test_hex[,17])
+# > confusion_pred
+# Predicted
+# Actual     no yes Error
+# no     3630 433 0.107
+# yes     264 265 0.499
+# Totals 3894 698 0.152
+# > 
+#   > # Get the confusion Matrix - Training Confustion Matrix
+#   > confusion_train = h2o.confusionMatrix(prediction_train[,1], train_hex[,17])
+# > confusion_train
+# Predicted
+# Actual      no  yes Error
+# no     32063 3796 0.106
+# yes     2158 2602 0.453
+# Totals 34221 6398 0.147
