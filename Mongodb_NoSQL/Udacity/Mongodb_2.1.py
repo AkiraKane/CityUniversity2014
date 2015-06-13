bdde4bfbe37d371d08574a302b07331cb4950788
dandxy89 <dan.dixey@gmail.com>
2015-06-13 19:05:25 +0100

    Update

---
+++ Mongodb_NoSQL/Udacity/Mongodb_2.1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 17:09:17 2015
+
+@author: dan
+"""
+
+from pymongo import MongoClient
+import pprint
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client.examples
+
+def find():
+    autos = db.autos.find(
+        { "manufacturer" : "Toyota",
+          "class" : "mid-size car"})
+    for a in autos:
+        pprint.pprint(a)
+
+if __name__ == '__main__':
+    find()
