1d83bbf76dbbcfda72e6d041d3a7ce55c77ff38c
dan.dixey <dan.dixey@gmail.com>
2015-10-11 12:03:43 +0100

    Starting ML CW Script

---
+++ Machine_Learning/Rossmann_Store_Sales/coursework_script.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+# Created: 11/10/2015
+# Last Modified: 11/10/2015
+# Dan Dixey
+# Machine Learning Coursework
+
+import pandas as pd
+import numpy as np
+from time import time
+import os
+
+
+def importData(name):
+    assert isinstance(name, str), 'Enter the NAME of the File to be imported!'
+    fileName = "/".join([os.getcwd(),name])
+    print "Importing '{}' as a Dataframe".format(name)
+    return pd.DataFrame.from_csv(fileName)
+    
+
+def main():
+    # Import the Data
+    trainingDF = importData('train.csv')
+    testDF = importData('test.csv')
+    storesDF = importData('store.csv')
+    # Data Preparation
+        # Joins, to Numpy Arrays, Categorical to Numerical
+    # Basic Statistics
+        # Missing Data - Table + Chart
+        # Column Statistics - Table + Chart
+        # Summary table
+        # Data dictionary
+    # Preprocessing Steps
+        # Removing Spurious Data
+        # Sampling, Setting up Cross Validation
+    # Machine Learning Processing
+        # ID2 ML Algorithms
+        # Optimisation Wrapper
+    # Access Results
+        # Feature Engineering
+        # Evaluate Models
+    # Results and Metrics
+        # Typical Regression Metrics - Kaggle
+        # Graphical and Tabular Results
+    return
+
+if __name__ == "main":
+    main()
