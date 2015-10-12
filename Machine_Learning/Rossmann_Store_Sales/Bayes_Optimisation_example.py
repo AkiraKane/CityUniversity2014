4d20ef1f788fd495231d234c64b0d5c55500d79d
dan.dixey <dan.dixey@gmail.com>
2015-10-12 22:08:32 +0100

    Bayes Update

--- Machine_Learning/Rossmann_Store_Sales/Bayes_Optimisation_example.py
+++ Machine_Learning/Rossmann_Store_Sales/Bayes_Optimisation_example.py
@@ -56,14 +56,15 @@ def f(params):
 '''
 startT = time()
 X, y = import_data()
-space4dt = dict(max_depth=hp.choice('max_depth', range(1, 20)),
+space4dt = dict(max_depth=hp.choice('max_depth', range(1, 30)),
                 max_features=hp.choice('max_features', range(1, 5)),
                 criterion=hp.choice('criterion', ["gini", "entropy"]),
                 normalize=hp.choice('normalize', [0, 1]))
 
 trials = Trials()
-best = fmin(f, space4dt, algo=tpe.suggest, max_evals=300, trials=trials)
-print 'Best Solution (Parameters): {}'.format(best)
+best = fmin(f, space4dt, algo=tpe.suggest, max_evals=900, trials=trials)
+print 'Best Solution (Parameters): {} Loss (Result): {}'.format(best, 
+                        np.abs(trials.best_trial['result']['loss']))
 
 print('Plotting Parameters by Loss')
 parameters = ['max_depth', 'max_features', 'criterion', 'normalize']
@@ -78,6 +79,7 @@ for i, val in enumerate(parameters):
     axes[i].scatter(xs, ys, s=20, linewidth=0.01, alpha=0.4, c=cmap(float(i)/len(parameters)))
     axes[i].set_title(val)
     axes[i].set_ylim([0.8,1])
+plt.show()
 
 print('Plotting Trail by Loss')
 f, ax = plt.subplots(1)
@@ -88,4 +90,5 @@ ax.scatter(xs, ys, s=20, linewidth=0.01, alpha=0.75)
 ax.set_title('Loss vs Trial Number ', fontsize=18)
 ax.set_xlabel('Trial Number', fontsize=16)
 ax.set_ylabel('Model Loss', fontsize=16)
-print('Total time to Load, Optimise and Present Details: {}seconds').format(time() - startT)
+plt.show()
+print('Total time to Load, Optimise and Present Details: {} seconds').format(time() - startT)
